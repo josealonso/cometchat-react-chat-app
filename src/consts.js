9025c360985981e9ac6708d942662c0275a4c145
josealonso <jose.alonso.programmer@gmail.com>
2022-04-07 00:50:40 +0200

    Added my cometchat keys

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,7 @@
 export const COMETCHAT_CONSTANTS = {
-	APP_ID: "APP_ID",
-	REGION: "REGION",
-	AUTH_KEY: "AUTH_KEY",
+	APP_ID: "206866f2357d53d0", // process.env.APP_ID || "",
+	REGION: "eu", // process.env.REGION || "",
+	AUTH_KEY: "8142f0af650fe9c5d4414e25b87ad0fc1b78a21a" // process.env.AUTH_KEY || ""
 };
+
+
